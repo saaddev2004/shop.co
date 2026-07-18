75251b91cbdfb00b21a37e2218632c8417936720
Muhammad Saad <124339063+saaddev2004@users.noreply.github.com>
2026-07-18 18:48:31 +0500

    Implement loading state in HomePage
    Added loading state handling to HomePage component.

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-
+import { useProducts } from "../../Context/ProductContext";
 
 import HeroSection from "../../components/hero/HeroSection";
 import BrandStrip from "../../components/BrandStrip/BrandStrip";
@@ -8,13 +7,19 @@ import NewArrivals from "../../components/NewArrivals/NewArrivals";
 import SaleSection from "../../components/SaleSection/SaleSection";
 import Testimonials from "../../components/Testimonials/Testimonials";
 
-
 function HomePage() {
+  const { loading } = useProducts();
 
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-black dark:border-white border-b-transparent"></div>
+      </div>
+    );
+  }
 
   return (
     <div>
-
       <HeroSection />
       <BrandStrip />
 
@@ -25,7 +30,6 @@ function HomePage() {
       <SaleSection />
 
       <Testimonials />
-
     </div>
   );
 }
